2aa8774511c2365bb47a87adfb057b07b6953a5f
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-11 13:31:42 +0300

    Add Layout component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import React from 'react';
+import Layout from '@/components/Layout';
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col">
+      <Layout />
+      
       
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
