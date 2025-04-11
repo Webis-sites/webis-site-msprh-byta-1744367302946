4bdeff4ceba881b6c7995b735c99aa4844a54bd2
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-11 13:57:57 +0300

    Add Card component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,13 @@ import LocationSection from '@/components/LocationSection';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import Button from '@/components/Button';
+import Card from '@/components/Card';
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col">
+      <Card />
+      
       <Button />
       
       <Navbar />
